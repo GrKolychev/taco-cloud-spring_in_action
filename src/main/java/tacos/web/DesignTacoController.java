e8b64b32585a1866b811a470153b2993c3814350
GrKolychev <kolychevgv92@gmail.com>
2022-03-30 01:04:02 +0300

    Chapter 2.3 Validating form input

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -3,9 +3,11 @@ package tacos.web;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,7 +52,10 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(final Taco taco) {
+    public String processTaco(@Valid @ModelAttribute("taco") final Taco taco, final Errors errors) {
+        if (errors.hasErrors()){
+            return "design";
+        }
         // Save the taco...
         // We'll do this in chapter 3
         log.info("Processing taco: " + taco);
