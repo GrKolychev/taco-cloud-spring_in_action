81b621323dca2e43e110b64c1d125b2e710e036b
GrKolychev <kolychevgv92@gmail.com>
2022-06-15 16:42:19 +0300

    Chapter 3.1.3 - 3.1.4

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -8,12 +8,21 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import tacos.TacoOrder;
+import tacos.data.OrderRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
 public class OrderController {
+
+    private OrderRepository orderRepo;
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
 
     @GetMapping("/current")
     public String orderForm(final Model model) {
@@ -22,10 +31,12 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(@Valid final TacoOrder order, final Errors errors) {
+    public String processOrder(@Valid final TacoOrder order, final Errors errors, final SessionStatus sessionStatus) {
         if (errors.hasErrors()){
             return "orderForm";
         }
+        orderRepo.save(order);
+        sessionStatus.setComplete();
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
